45b63f0f483032a129b45bc5da78e06a36efd060
boyanatsvetkova <boyana.tsvetkova@gmail.com>
2016-02-24 01:01:22 +0200

    final design and error pages done

---
+++ Web/SnapCot.Web/Controllers/ErrorController.cs
@@ -0,0 +1,22 @@
+﻿namespace SnapCot.Web.Controllers
+{
+    using System.Web.Mvc;
+
+    public class ErrorController : Controller
+    {
+        public ActionResult Error400()
+        {
+            return View();
+        }
+  
+        public ActionResult Error404()
+        {
+            return View();
+        }
+
+        public ActionResult Error500()
+        {
+            return View();
+        }
+    }
+}
